cdde7afb9a9cbf2169e2d5eae2b3567cd72c6666
chenwei <chenweiwool@163.com>
2022-08-16 20:48:39 +0800

    feat:开发布局组件和引入charts组件

---
+++ src/components/Charts/rouseChart.vue
@@ -0,0 +1,110 @@
+<template>
+    <echart :option="lineChartOption"></echart>
+    <!-- <div id="charts-box"></div> -->
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import utils from "@/public/utils.js";
+import api from "@/public/api.js";
+import $ from 'jquery';
+import echart from './echart.vue'
+
+export default defineComponent({
+    name: 'lineChart',
+    components: {
+        echart
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'title'
+        }
+    },
+    setup(props, { emit }) {
+
+        const lineChartOption = {
+            grid: {
+                show: true,
+                left: 10,
+                right: 10,
+                top: 10,
+                bottom: 10
+            },
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b} : {c} ({d}%)'
+            },
+            legend: {
+                left: 'center',
+                top: 'bottom',
+                data: [
+                'rose1',
+                'rose2',
+                'rose3',
+                'rose4',
+                'rose5',
+                'rose6',
+                'rose7',
+                'rose8'
+                ]
+            },
+            series: [
+                {
+                    name: 'Radius Mode',
+                    type: 'pie',
+                    radius: [10, 80],
+                    center: ['50%', '50%'],
+                    roseType: 'radius',
+                    itemStyle: {
+                        borderRadius: 5
+                    },
+                    label: {
+                        show: false
+                    },
+                    emphasis: {
+                        label: {
+                        show: true
+                        }
+                    },
+                    data: [
+                        { value: 40, name: 'rose 1' },
+                        { value: 33, name: 'rose 2' },
+                        { value: 28, name: 'rose 3' },
+                        { value: 22, name: 'rose 4' },
+                        { value: 20, name: 'rose 5' },
+                        { value: 15, name: 'rose 6' },
+                        { value: 12, name: 'rose 7' },
+                        { value: 10, name: 'rose 8' }
+                    ]
+                }
+            ]
+        }
+        return {
+            lineChartOption
+        }
+    }
+})
+</script>
+
+<style lang="less" scoped>
+.item-box-container {
+    height: 100%;
+    width: 100%;
+    border: 1px solid red;
+
+    .item-title {
+        width: 100%;
+        height: 20px;
+        color: red;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .item-content-container {
+        width: 100%;
+        height: calc(100% - 20px);
+    }
+}
+</style>
---
+++ src/components/ItemBox.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="item-box-container">
+        <div class="item-title">{{ title }}</div>
+        <div class="item-content-container">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import utils from "../public/utils.js";
+import api from "../public/api.js";
+import $ from 'jquery';
+import * as echarts from 'echarts'
+
+export default defineComponent({
+    name: 'itemBox',
+    components: {},
+    props: {
+        title: {
+            type: String,
+            default: 'title'
+        }
+    },
+    setup(props, { emit }) {
+    }
+})
+</script>
+
+<style lang="less" scoped>
+.item-box-container {
+    height: 100%;
+    width: 100%;
+    border: 1px solid red;
+
+    .item-title {
+        width: 100%;
+        height: 20px;
+        color: red;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .item-content-container {
+        width: 100%;
+        height: calc(100% - 20px);
+    }
+}
+</style>
